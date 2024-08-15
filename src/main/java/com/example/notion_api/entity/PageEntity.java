5f1a92a3e7b1dbd14328993fbc5df4a3b0925458
JeongBanSeok <wibalislfkas@naver.com>
2024-08-15 20:36:41 +0900

    Entity 객체 생성

---
+++ src/main/java/com/example/notion_api/entity/PageEntity.java
@@ -0,0 +1,14 @@
+package com.example.notion_api.entity;
+
+import java.time.LocalDateTime;
+
+public class PageEntity {
+
+    private String userId;
+
+    private String title;
+
+    private LocalDateTime updatedDate;
+
+    private String content;
+}
---
+++ src/main/java/com/example/notion_api/entity/pageTemplateEntity.java
@@ -0,0 +1,14 @@
+package com.example.notion_api.entity;
+
+public class pageTemplateEntity {
+
+    /**
+     * 페이지 생성 시 기본 템플릿
+     * 제공을 위한 엔티티
+     * */
+    private Long id;
+
+    private String title;
+
+    private String contentReference;
+}
