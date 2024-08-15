de23f547daf64fe9d37abc842ffb46ad6f570e55
JeongBanSeok <wibalislfkas@naver.com>
2024-08-15 15:27:16 +0900

    프로젝트 생성

---
+++ notion-api/src/test/java/com/example/notion_api/NotionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notion_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
