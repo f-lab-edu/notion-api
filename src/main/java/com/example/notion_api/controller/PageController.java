dfa8d75d8d3c9643f7cfd2bb37fabf318f0b6b20
JeongBanSeok <wibalislfkas@naver.com>
2024-08-16 02:22:08 +0900

    서비스를 이용한 PageController 내부 동작 구현

--- src/main/java/com/example/notion_api/controller/PageController.java
+++ src/main/java/com/example/notion_api/controller/PageController.java
@@ -1,9 +1,13 @@
 package com.example.notion_api.controller;
 
+import com.example.notion_api.dto.page.PageDTO;
 import com.example.notion_api.service.PageServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/api")
@@ -15,15 +19,15 @@ public class PageController {
      * page 생성 요청 처리
      * */
     @PostMapping("/newpage")
-    public String createDefaultPage(
+    public PageDTO createDefaultPage(
         @RequestParam String userId,
         @RequestParam String pageType
     ){
         /* pageType : to-do, weekplan, diary, table, board,
         *             list, timeline, calender, gallery
         * */
-        pageService.createPage(userId, pageType);
-        return "";
+        PageDTO pageDTO = pageService.createPage(userId, pageType);
+        return pageDTO;
     }
 
 
@@ -31,11 +35,12 @@ public class PageController {
      * 페이지 템플릿 반환(특정 페이지 선택시)
      * */
     @GetMapping("/page")
-    public String getPage(
+    public PageDTO getPage(
             @RequestParam String userId,
             @RequestParam String title
     ){
-        return "";
+        PageDTO pageDTO = pageService.getPage(userId, title);
+        return pageDTO;
     }
 
     /**
@@ -43,19 +48,26 @@ public class PageController {
      * (로컬과 서버의 업데이트 시간 비교)
      * */
     @PutMapping("/update-page/{userId}")
-    public String updatePage(
-            @PathVariable String userId
+    @ResponseBody
+    public ResponseEntity<List<PageDTO>> updatePage(
+            @PathVariable String userId,
+            @RequestBody List<PageDTO> pageDTOs
     ){
-        return "";
+        List<PageDTO> updatedPages = pageService.updatePages(userId,pageDTOs);
+
+        return ResponseEntity.ok(updatedPages);
     }
 
     /**
      * 페이지 삭제
      * */
     @DeleteMapping("/delete-page/{userId}")
-    public String deletePage(
-            @PathVariable String userId
+    @ResponseBody
+    public ResponseEntity<String> deletePage(
+            @PathVariable String userId,
+            @RequestBody PageDTO pageDTO
     ){
-        return "";
+        pageService.deletePage(userId,pageDTO);
+        return ResponseEntity.ok("페이지가 정상적으로 삭제되었습니다.");
     }
 }
