98e932a5daca99c4c569a28bb32a182a42e057e3
JeongBanSeok <wibalislfkas@naver.com>
2024-08-22 13:25:38 -0700

    [10] Aws S3의 DAO 인터페이스 구현

--- src/main/java/com/example/notion_api/dao/AwsS3DAO.java
+++ src/main/java/com/example/notion_api/dao/AwsS3DAO.java
@@ -2,18 +2,19 @@ package com.example.notion_api.dao;
 
 import com.example.notion_api.dto.s3.S3FileDTO;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface AwsS3DAO {
 
     /** 아이콘, 이미지 파일 저장 */
-    void uploadLocalFile(String localPath, String remotePath, String fileName);
+    void uploadLocalFile(String localPath, String remotePath, String fileName) throws IOException;
 
     /** 페이지 생성, 기본 템플릿 페이지 생성 */
-    void uploadStringAsFile(String remotePath, String fileName, String content);
+    void uploadStringAsFile(String remotePath, String fileName, String content) throws IOException;
 
     /**  */
-    String downloadFileAsString(String remotePath, String fileName);
+    String downloadFileAsString(String remotePath, String fileName) throws IOException;
 
     /** 페이지 삭제 */
     void deleteFile(String remotePath, String fileName);
@@ -22,8 +23,8 @@ public interface AwsS3DAO {
     List<String> getFileNames(String remotePath, String prefix);
 
     /** 로그인 시 페이지 업데이트, 특정 시간마다 페이지 업데이트 */
-    void updateStringAsFile(String remotePath, String fileName, String content);
+    void updateStringAsFile(String remotePath, String fileName, String content) throws IOException;
 
     /** 페이지 가져오기, 버전별 페이지 가져오기 */
-    S3FileDTO getFileNameAndContent(String remotePaht, String prefix);
+    S3FileDTO getFileNameAndContent(String remotePaht, String prefix) throws IOException;
 }
