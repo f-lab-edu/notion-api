5306d8d7f5d213266cbd5679e3b994661765d2f2
JeongBanSeok <wibalislfkas@naver.com>
2024-08-23 19:39:54 +0900

    [24] 페이지 업데이트 및 반환하는 서비스 구현

--- src/main/java/com/example/notion_api/service/PageService.java
+++ src/main/java/com/example/notion_api/service/PageService.java
@@ -15,9 +15,7 @@ public interface PageService {
 
     PageDTO getPage(String userId,PageDTO pageDTO) throws IOException;
 
-    List<PageDTO> getUpdatedPageByLogin(String userId, List<PageDTO> pageDTOs);
-
-    List<PageDTO> getUpdatedPageByTimer(String userId, List<PageDTO> pageDTOs);
+    List<PageDTO> getUpdatedPage(String userId, List<PageDTO> pageDTOs) throws IOException;
 
     void deletePage(String userId, String pageId);
 
