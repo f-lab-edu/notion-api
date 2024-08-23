caa5cc0733ff1cb80b505282dae9568277dcbe2f
JeongBanSeok <wibalislfkas@naver.com>
2024-08-23 01:48:26 -0700

    [16] 기본 템플릿의 마크다운을 db가 아닌 메모리에서 읽어오기(속도 향상)

--- src/main/java/com/example/notion_api/markdown/MarkdownTemplate.java
+++ src/main/java/com/example/notion_api/markdown/MarkdownTemplate.java
@@ -32,5 +32,53 @@ public class MarkdownTemplate {
             "<n-page title=\"페이지1\", createddate=\"{createddate}\", tag=\"\",taghint=\"비어있음\"/>\n"+
             "<n-page title=\"페이지2\", createddate=\"{createddate}\", tag=\"\",taghint=\"비어있음\"/>\n"+
             "<n-page title=\"페이지3\", createddate=\"{createddate}\", tag=\"\",taghint=\"비어있음\"/>\n";
-
+    public static final String MARKDOWN_TEMPLATE_TABLE =
+           "<n-title font=\"bold\",sort=\"left\",hint=\"제목없음\",text=\"\"/>\n"+
+            "<n-table-head title=\"표\",column=\"2\", type1=\"title\", text1=\"이름\", type2=\"multichoice\",text2=\"태그\"/>\n"+
+            "<n-table-content column=\"2\", text1=\"\", text2=\"\"/>\n"+
+            "<n-table-content column=\"2\", text1=\"\", text2=\"\"/>\n"+
+            "<n-table-content column=\"2\", text1=\"\", text2=\"\"/>\n";
+    public static final String MARKDOWN_TEMPLATE_TIMELINE =
+            "<n-title font=\"bold\",sort=\"left\",hint=\"제목없음\",text=\"\"/>\n"+
+            "<n-timeline type=\"title\", title=\"타임라인 보기\"/>\n"+
+            "<n-board-content title=\"카드1\", type1=\"date\",text=\"{todaydate}{date}\",type2=\"people\",text=\"\",type3=\"state\",text=\"시작전\"/>\n"+
+            "<n-board-content title=\"카드2\", type1=\"date\",text=\"{todaydate}{date}\",type2=\"people\",text=\"\",type3=\"state\",text=\"시작전\"/>\n"+
+            "<n-board-content title=\"카드3\", type1=\"date\",text=\"{todaydate}{date}\",type2=\"people\",text=\"\",type3=\"state\",text=\"시작전\"/>\n";
+    public static final String MARKDOWN_TEMPLATE_TODO =
+            "<n-title font=\"bold\",sort=\"left\",hint=\"제목없음\",text=\"할 일 목록\"/>\n"+
+            "<n-icon path = \"...\">\n"+
+            "<n-cover path = \"...\">\n"+
+            "<n-content type=\"todo\", state=\"unchecked\", hint=\"할 일\",text=\"\"/>\n"+
+            "<n-content type=\"todo\", state=\"unchecked\", hint=\"할 일\",text=\"\"/>\n"+
+            "<n-content type=\"todo\", state=\"unchecked\", hint=\"할 일\",text=\"\"/>\n"+
+            "<n-content type=\"todo\", state=\"unchecked\", hint=\"할 일\",text=\"\"/>\n";
+    public static final String MARKDOWN_TEMPLATE_WEEKPLAN =
+            "<n-title font=\"bold\",sort=\"left\",hint=\"제목없음\",text=\"{date}\"/>\n"+
+            "<n-icon path = \"...\">\n"+
+            "<n-cover path = \"...\">\n"+
+            "<n-content type=\"titletoggle1\" text=\"월요일\">\n"+
+            "<n-content type=\"todo\", state=\"unchecked\", hint=\"할 일\",text=\"...\"/>\n"+
+            "<n-content type=\"todo\", state=\"unchecked\", hint=\"할 일\",text=\"...\"/>\n"+
+            "<n-content type=\"todo\", state=\"unchecked\", hint=\"할 일\",text=\"...\"/>\n"+
+            "</n-content>\n"+
+            "<n-content type=\"titletoggle1\" text=\"화요일\">\n"+
+            "<n-content type=\"todo\", state=\"unchecked\", hint=\"할 일\",text=\"...\"/>\n"+
+            "<n-content type=\"todo\", state=\"unchecked\", hint=\"할 일\",text=\"...\"/>\n"+
+            "<n-content type=\"todo\", state=\"unchecked\", hint=\"할 일\",text=\"...\"/>\n"+
+            "</n-content>\n"+
+            "<n-content type=\"titletoggle1\" text=\"수요일\">\n"+
+            "<n-content type=\"todo\", state=\"unchecked\", hint=\"할 일\",text=\"...\"/>\n"+
+            "<n-content type=\"todo\", state=\"unchecked\", hint=\"할 일\",text=\"...\"/>\n"+
+            "<n-content type=\"todo\", state=\"unchecked\", hint=\"할 일\",text=\"...\"/>\n"+
+            "</n-content>\n"+
+            "<n-content type=\"titletoggle1\" text=\"목요일\">\n"+
+            "<n-content type=\"todo\", state=\"unchecked\", hint=\"할 일\",text=\"...\"/>\n"+
+            "<n-content type=\"todo\", state=\"unchecked\", hint=\"할 일\",text=\"...\"/>\n"+
+            "<n-content type=\"todo\", state=\"unchecked\", hint=\"할 일\",text=\"...\"/>\n"+
+            "</n-content>\n"+
+            "<n-content type=\"titletoggle1\" text=\"금요일\">\n"+
+            "<n-content type=\"todo\", state=\"unchecked\", hint=\"할 일\",text=\"...\"/>\n"+
+            "<n-content type=\"todo\", state=\"unchecked\", hint=\"할 일\",text=\"...\"/>\n"+
+            "<n-content type=\"todo\", state=\"unchecked\", hint=\"할 일\",text=\"...\"/>\n"+
+            "</n-content>\n";
 }
