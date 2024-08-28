6d7c0aa645084a78688948e6ef1c19dccae6bf04
JeongBanSeok <wibalislfkas@naver.com>
2024-08-28 15:46:37 +0900

    [32] OAuth2 사용을 위한 config 설정, 의존성 추가

--- src/main/java/com/example/notion_api/config/SecurityConfig.java
+++ src/main/java/com/example/notion_api/config/SecurityConfig.java
@@ -1,13 +1,41 @@
 package com.example.notion_api.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+
+        // csrf 비활성화
+        httpSecurity.csrf((auth) -> auth.disable());
+
+        // Form 로그인 방식 비활성화
+        httpSecurity.formLogin((auth) -> auth.disable());
+
+        // HTTP Basic 인증 방식 비활성화
+        httpSecurity.httpBasic((auth) -> auth.disable());
+
+        // oauth2(디폴트 설정)
+        httpSecurity.oauth2Login(Customizer.withDefaults());
+
+        // 경로별 인가 작업
+        httpSecurity.authorizeHttpRequests((auth) -> auth
+                .requestMatchers("/").permitAll()
+                .anyRequest().authenticated());
+
+        // 세션 설정 : STABLELESS
+        httpSecurity.sessionManagement((session) -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return httpSecurity.build();
     }
 }
