0616ec90bc74f1b60356dc0cde60950a58ccb73a
JeongBanSeok <wibalislfkas@naver.com>
2024-08-30 22:58:43 +0900

    [37] 유저 DB 설정

---
+++ src/main/java/com/example/notion_api/entity/UserEntity.java
@@ -0,0 +1,23 @@
+package com.example.notion_api.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String name;
+    private String email;
+    private String role;
+
+}
--- src/main/java/com/example/notion_api/service/CustomOAuth2UserService.java
+++ src/main/java/com/example/notion_api/service/CustomOAuth2UserService.java
@@ -4,6 +4,8 @@ import com.example.notion_api.dto.oauth.CustomOAuth2User;
 import com.example.notion_api.dto.oauth.GoogleResponse;
 import com.example.notion_api.dto.oauth.OAuth2Response;
 import com.example.notion_api.dto.oauth.UserDTO;
+import com.example.notion_api.entity.UserEntity;
+import com.example.notion_api.repository.UserRepository;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
@@ -14,6 +16,12 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    private final UserRepository userRepository;
+
+    public CustomOAuth2UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -29,10 +37,39 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
         String username = oAuth2Response.getProvider()+" "+oAuth2Response.getProviderId();
 
-        UserDTO userDTO = new UserDTO();
-        userDTO.setUsername(username);
-        userDTO.setName(oAuth2Response.getName());
-        userDTO.setRole("ROLE_USER");
-        return new CustomOAuth2User(userDTO);
+        UserEntity existData = userRepository.findByUsername(username);
+
+        if (existData == null){
+            // 로그인 한 적이 없는 경우
+            UserEntity userEntity = new UserEntity();
+            userEntity.setUsername(username);
+            userEntity.setEmail(oAuth2Response.getEmail());
+            userEntity.setName(oAuth2Response.getName());
+            userEntity.setRole("ROLE_USER");
+
+            userRepository.save(userEntity);
+
+            UserDTO userDTO = new UserDTO();
+            userDTO.setUsername(username);
+            userDTO.setName(oAuth2Response.getName());
+            userDTO.setRole("ROLE_USER");
+
+            return new CustomOAuth2User(userDTO);
+        }else {
+            // 한번이라도 로그인한 경우
+            existData.setEmail(oAuth2Response.getEmail());
+            existData.setName(oAuth2Response.getName());
+
+            userRepository.save(existData);
+
+            UserDTO userDTO = new UserDTO();
+            userDTO.setUsername(existData.getUsername());
+            userDTO.setName(oAuth2Response.getName());
+            userDTO.setRole(existData.getRole());
+
+            return new CustomOAuth2User(userDTO);
+        }
+
+
     }
 }
